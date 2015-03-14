86dad6885cceb37d0696064c8c91d0e5e0567467
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-03-14 22:45:18 +0100

    Add argument validation helper utility

---
+++ src/FSharp.Data.Xml/Argument.fs
@@ -0,0 +1,19 @@
+﻿namespace FSharp.Data.Xml
+
+open System
+
+module internal Argument =
+
+    let validateNotNull value paramName =
+        if value = null then raise (ArgumentNullException(paramName))
+
+    let validateNotNullOrEmpty value (paramName : string) =
+        if String.IsNullOrEmpty(value)
+        then raise (ArgumentException("The value cannot be null or empty.", paramName))
+
+    let validateNotNullOrWhitespace value (paramName : string) =
+        if String.IsNullOrWhiteSpace(value)
+        then raise (ArgumentException("The value cannot be null or whitespace.", paramName))
+
+    let validate condition (paramName : string) (message : string) =
+        if not(condition) then raise (ArgumentException(message, paramName))
