0c90d6d1b8cc6e25f2c5356dfad33cd4e929b2cc
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-03-14 22:23:30 +0100

    Create FSharp.Data.Xml project

---
+++ src/FSharp.Data.Xml.Tests/AssemblyInfo.fs
@@ -0,0 +1,34 @@
+﻿namespace FSharp.Data.Xml
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharp.Data.Xml.Tests")>]
+[<assembly: AssemblyDescription("Unit tests for FSharp.Data.Xml library")>]
+[<assembly: AssemblyProduct("FSharp.Data.Xml")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("4e6c9820-0862-47bc-ad21-37b55cfe5196")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.7.*")]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
---
+++ src/FSharp.Data.Xml/AssemblyInfo.fs
@@ -0,0 +1,38 @@
+﻿namespace FSharp.Data.Xml
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharp.Data.Xml")>]
+[<assembly: AssemblyDescription("Library for using XML data in F#")>]
+[<assembly: AssemblyProduct("FSharp.Data.Xml")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9f742f30-b0e0-4417-873a-234e0b709b5a")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.7.*")]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+// Expose internal members to unit tests project
+[<assembly: InternalsVisibleTo("FSharp.Data.Xml.Tests")>]
+
+()
