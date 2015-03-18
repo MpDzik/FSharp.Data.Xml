2a6f8b48452a4e55350c27967466487c661d4809
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-03-18 16:13:32 +0100

    Active patterns for matching XML nodes
    This adds active patterns for matching XML nodes which represent XML
    documents, elements and attributes.

---
+++ src/FSharp.Data.Xml.Tests/XmlActivePatternTests.fs
@@ -0,0 +1,58 @@
+﻿namespace FSharp.Data.Xml.Tests
+
+open System.Xml
+open FSharp.Data.Xml
+open Xunit
+
+type XmlActivePatternTests() =
+
+    let xml = @"<?xml version='1.0' encoding='utf-8'?>
+                <root>
+                    <foo bar='bar-value' baz='baz-value' />
+                </root>"
+
+    [<Fact>]
+    let ``Match document``() =
+        let document = Xml.ofString xml
+        let element = document |> Xml.querySingle "/root"
+        Assert.Equal(document, match document with XmlDocument d -> d | _ -> null)
+        Assert.Null(match element with XmlDocument d -> d | _ -> null)
+
+    let testNodes =
+        let rec getNodesRec (node : XmlNode) =
+            seq { yield node
+                  for attr in node.Attributes do yield attr
+                  for child in node.ChildNodes do yield! (getNodesRec child) }
+        getNodesRec (Xml.ofString xml |> Xml.root)
+
+    [<Fact>]
+    let ``Match element``() =
+        let result : string seq = 
+            seq { for node in testNodes do match node with XmlElement e -> yield Some e | _ -> yield None }
+                |> Seq.filter (fun r -> r.IsSome)
+                |> Seq.map (fun r -> r.Value.Name)
+        Assert.Equal([| "root"; "foo" |], result)
+
+    [<Fact>]
+    let ``Match element by name``() =
+        let result : string seq = 
+            seq { for node in testNodes do match node with XmlElem("foo") e -> yield Some e | _ -> yield None }
+                |> Seq.filter (fun r -> r.IsSome)
+                |> Seq.map (fun r -> r.Value.Name)
+        Assert.Equal([| "foo" |], result)
+
+    [<Fact>]
+    let ``Match attribute``() =
+        let result : string seq = 
+            seq { for node in testNodes do match node with XmlAttribute a -> yield Some a | _ -> yield None }
+                |> Seq.filter (fun r -> r.IsSome)
+                |> Seq.map (fun r -> r.Value.Name)
+        Assert.Equal([| "bar"; "baz" |], result)
+
+    [<Fact>]
+    let ``Match attribute by name``() =
+        let result : string seq = 
+            seq { for node in testNodes do match node with XmlAttr("baz") e -> yield Some e | _ -> yield None }
+                |> Seq.filter (fun r -> r.IsSome)
+                |> Seq.map (fun r -> r.Value.Name)
+        Assert.Equal([| "baz" |], result)
---
+++ src/FSharp.Data.Xml/XmlActivePatterns.fs
@@ -0,0 +1,36 @@
+﻿namespace FSharp.Data.Xml
+
+open System.Xml
+
+[<AutoOpen>]
+module XmlActivePatterns = 
+
+    /// Matches any XML document node
+    let (|XmlDocument|_|) (node : XmlNode) =
+        match node with
+        | n when n.NodeType = XmlNodeType.Document -> Some(node :?> XmlDocument)
+        | _ -> None
+
+    /// Matches any XML element node
+    let (|XmlElement|_|) (node : XmlNode) =
+        match node with
+        | n when n.NodeType = XmlNodeType.Element -> Some(node :?> XmlElement)
+        | _ -> None
+
+    /// Matches any XML element node with the specified name
+    let (|XmlElem|_|) (name : string) (node : XmlNode) =
+        match node with
+        | n when n.NodeType = XmlNodeType.Element && n.Name = name -> Some(node :?> XmlElement)
+        | _ -> None
+
+    /// Matches any XML attribute node
+    let (|XmlAttribute|_|) (node : XmlNode) =
+        match node with
+        | n when n.NodeType = XmlNodeType.Attribute -> Some(node :?> XmlAttribute)
+        | _ -> None
+
+    /// Matches any XML attribute node with the specified name
+    let (|XmlAttr|_|) (name : string) (node : XmlNode) =
+        match node with
+        | n when n.NodeType = XmlNodeType.Attribute && n.Name = name -> Some(node :?> XmlAttribute)
+        | _ -> None
